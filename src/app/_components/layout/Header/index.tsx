8353a007b48016c9312e7a9dfa2466dd257ccbdb
rnogara <r.nogara.dev@gmail.com>
2024-11-01 18:16:52 -0300

    change some sizes and effects on header and home page bg

--- src/app/_components/layout/Header/index.tsx
+++ src/app/_components/layout/Header/index.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="relative z-30">
       <div className="fixed top-0 left-0 w-full h-16 flex justify-between items-center px-8">
-        <p className="text-3xl">
+        <p className="text-3xl drop-shadow-2xl">
           Huang Go
         </p>
         <Menu />
--- src/app/_components/layout/HomePage.tsx
+++ src/app/_components/layout/HomePage.tsx
@@ -4,7 +4,13 @@ export default function HomePage() {
   return (
     <div className="relative">
       <div className="absolute h-svh w-full top-0 left-0">
-        <Image src="/images/bg-home.png" width={3000} height={800} alt="imagem do Mt. Fuji breto e branco" className="object-fill h-full w-full -z-10" />
+        <Image
+          src="/images/bg-home.png"
+          width={3000}
+          height={800}
+          alt="Imagem do Mt. Fuji breto e branco"
+          className="object-cover h-full w-full -z-10"
+        />
       </div>
     </div>
   );
