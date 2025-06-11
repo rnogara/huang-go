087d0a0bdd9078992b5b0096c10e84c73e38ba3d
rnogara <r.nogara.dev@gmail.com>
2025-06-11 17:30:37 -0300

    Integrate Analytics component into RootLayout for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
       <body className="bg-black">
         <TRPCReactProvider>
           {children}
+          <Analytics />
         </TRPCReactProvider>
       </body>
     </html>
